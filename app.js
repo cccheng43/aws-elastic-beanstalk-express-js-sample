b04c9779e7564a15155adc701f5db4125717a327
cccheng43 <cccheng43@gmail.com>
2021-01-18 12:01:56 +1100

    Full pipeline test

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hello my friends!'));
+app.get('/', (req, res) => res.send('Hello my friends! This is for testing the CD pipeline with AWS.'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
